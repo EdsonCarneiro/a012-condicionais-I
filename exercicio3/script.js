5d531d3ca8ee8144fe04be4b79479a93b179284c
EdsonCarneiro <edsonrje@gmail.com>
2022-08-04 18:33:01 -0300

    EXERCICIO-A012

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual a sua nacionalidade?")
+
+if (nacionalidade === "brasileira"){
+    console.log('Você é brasileiro');
+} else if (nacionalidade === 'argentina' ){
+    console.log('Você é argentino');
+}else if (nacionalidade === 'uruguaia' ){
+    console.log('Você é uruguaio');
+}else if (nacionalidade === 'chilena' ){
+    console.log('Você é chileno');
+}else if (nacionalidade === 'colombiana' ){
+    console.log('Você é colombiano');
+}else {
+console.log('nacionalidade não encontrada');
+}
